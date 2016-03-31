28f77e5e208f7cfa065dc991c51e6b83869e725a
Bob Massarczyk <bob@b2m9.com>
2016-03-31 21:11:32 +0000

    Setup
    - Add gitignore
    - Add npm packages
    - Add Grunt jobs to minify HTML, CSS, JS, and images

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+    var jpegOptim = require("imagemin-jpegoptim");
+    var pngZopfli = require("imagemin-zopfli");
+    var pngQuant = require("imagemin-pngquant");
+
+    grunt.initConfig({
+        htmlmin: {
+            main: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: "src/",
+                    src: ["**/*.html"],
+                    dest: "dist/"
+                }]
+            }
+        },
+        cssmin: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: "src/",
+                    src: ["**/*.css"],
+                    dest: "dist/"
+                }]
+            }
+        },
+        uglify: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: "src/",
+                    src: ["**/*.js"],
+                    dest: "dist/"
+                }]
+            }
+        },
+        imagemin: {
+            main: {
+                options: {
+                    optimizationLevel: 5,
+                    progressive: true,
+                    use: [
+                        jpegOptim({
+                            progressive: true,
+                            max: 80
+                        }),
+                        pngZopfli(),
+                        pngQuant()
+                    ]
+                },
+                files: [{
+                    expand: true,
+                    cwd: "src/",
+                    src: ["**/*.{png,jpg}"],
+                    dest: "dist/"
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-htmlmin");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-imagemin");
+
+    grunt.registerTask("build", ["htmlmin", "cssmin", "uglify", "imagemin"]);
+};
